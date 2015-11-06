f40c635fe1450ae99b553f7036fc8cdd9949d285
Omar Sharifali <osharifali@gmail.com>
2015-11-05 21:23:15 -0500

    Added first 5 basic operations
    basic matrix operations done

--- Matrix.java
+++ Matrix.java
@@ -1,47 +1,69 @@
 public class Matrix {
-	private double[][] matrix;
-	private double[][] vector;
-
-	public Matrix(double[][] matrix) {
-		this.matrix = matrix;
-	}
-	public Matrix(double[] vector) {
-		this.vector = vector;
-	}
-	public double getElement(int i, int j) {
-		return matrix[i][j];
-	}
-	public void setElement(int i, int j, double data) {
-		matrix[i][j] = data;
-	}
-
-	public add(Matrix m2) {
-		Matrix m3;
+	public double[][] add(double[][] m1, double[][] m2) {
+		double[][] m3;
 		for (int i = 0; i < m1.length; i++) {
 			for (int j = 0; j < m1[0].length, j++) {
-				m3[i][j] = m1[i][j] + m2[i][j];
+				m3[i][j] = m1[i][j] + m2[i][j];	
 			}
 		}
+		return m3;
 	}
-	public add(double[] v1, double[] v2) {
+	public double[] add(double[] v1, double[] v2) {
 		double[] v3;
 		for (int i = 0; i < v1.length; i++) {
 			v3 = v1[i] + v2[i];
 		}
+		return v3;
 	}
-	public subtract(double[][] m1, double[][] m2) {
+	public double[][] subtract(double[][] m1, double[][] m2) {
 		double[][] m3;
 		for (int i = 0; i < m1.length; i++) {
 			for (int j = 0; j < m1[0].length, j++) {
 				m3[i][j] = m1[i][j] - m2[i][j];
 			}
 		}
+		return m3;
 	}
 	public subtract(double[] v1, double[] v2) {
 		double[] v3;
 		for (int i = 0; i < v1.length; i++) {
 			v3 = v1[i] - v2[i];
 		}
+		return v3;
 	}
+	public double[][] scalarMultiply(int scalar, double[][] m1) {
+		for (int i = 0; i < m1.length; i++) {
+			for (int j = 0; j < m1[0].length; j++) {
+				m1[i][j] = scalar * m1[i][j];
+			}
+		}
+		return m1;
+	}
+	public double[] scalarMultiply(int scalar, double[] v1) {
+		for (int i = 0; i < v1.length; v++) {
+			v1[i] = scalar * v1[i];
+		}
+		return v1;
+	}
+	public double dotProduct(double[] v1, double[] v2) {
+		int dot;
+		for (int i = 0; i < v1.length; i++) {
+			dot += v1[i] * v2[i];
+		}
+		return dot;
+	}
+	public double[][] transpose(double[][] m1) {
+		double[][] m2 = m1;
+		for (int i = 0; i < m1.length; i++) {
+			for (int j = 0; j < m1[0].length; j++) {
+				m2[i][j] = m1[j][i];
+			}
+		}
+		return m2;
+	}
+	public double[] transpose(double[] v1) {
+		return v1;
+	}
+
 
 }
