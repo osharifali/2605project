b2a4d7a9151f460f0f3fbc8b7400e9dacf55015c
Omar Sharifali <osharifali@gmail.com>
2015-11-05 20:38:24 -0500

    Added a base matrix class
    very basic object creation

---
+++ Matrix.java
@@ -0,0 +1,11 @@
+public class Matrix {
+	private double[][] matrix;
+	private double[][] vector;
+
+	public Matrix(double[][] matrix) {
+		this.matrix = matrix;
+	}
+	public Matrix(double[] vector) {
+		this.vector = vector;
+	}
+}
