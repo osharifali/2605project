f51e3e1bd1825e6788adc91b16474512bec06220
mashalm <mashal.mannan@gmail.com>
2015-11-24 18:24:19 -0500

    fixed errors on iterations

--- GaussSeidel.java
+++ GaussSeidel.java
@@ -2,33 +2,43 @@
 public class GaussSeidel {
 	
 	public double[] gs_iter(double[][] a, double[] x, double[] b, double tolerance, double maxIter) {
-		double[] x0 = x;
-		double[][] s = a;
-		double[][] t = a;
-		for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                if (i >= j) {
-                    s[i][j] = a[i][j];
-                    t[i][j] = 0;
-                } else {
-                    s[i][j] = 0;
-                    t[i][j] = -1 * a[i][j];
-                }
-            }
-        }
+		double[][] diag = new double[a.length][a[0].length];
+		double[] x1 = x;
+		double[][] low = a;
+		double[][] upp = a;
+		for(int i = 0; i < a.length; i++) {
+			for(int j =0; j < a[0].length; j++) {
+				if(i==j)  {
+					diag[i][j] = a[i][j];
+					//upp[i][j] = a[i][j];
+					//low[i][j] = a[i][j];
+				}
+				if(i>j) {
+					upp[i][j] = a[i][j];
+					low[i][j] = 0.0;
+					diag[i][j] = 0.0;
+				} else {
+					low[i][j] = a[i][j];
+					upp[i][j] = 0.0;
+					diag[i][j] = 0.0;
+				}
+			}
+		}
 
         for(int i = 0; i < maxIter; i++) {
-        	x0 = x;
-        	double[] tx = Matrix.multiplyMatrixVector(x0, t);
-        	// x = Matrix.forwardSubstitution(s, Matrix.add(tx, b));
-        	double[] err = Matrix.subtract(x, x0);
+        	double[][] dplusl = Matrix.add(diag, low);
+        	double[][] negU = Matrix.scalarMultiply(-1, upp);
+        	double[] negUx = Matrix.multiplyMatrixVector(x, negU);
+        	double[] negUXplusb = Matrix.add(negUx, b);
+        	// x1 = Matrix.forwardSubstitution(dplusl, negUXplusb);
+        	double[] err = Matrix.subtract(x1, x);
         	if(Matrix.norm(err) < tolerance) {
         		System.out.println("converged");
-        		return x;
+        		return x1;
         	}
         } //fell out of loop w/o converging
         System.out.println("didn't converge in " + maxIter + " iterations");
-        return x;
+        return x1;
 
 	}
 }
--- JacobiIteration.java
+++ JacobiIteration.java
@@ -2,25 +2,25 @@
 public class JacobiIteration {
 	
 	public double[] jacobi_iter(double[][] a, double[] x, double[] b, double tolerance, int maxIter) {
-		double[][] diag = new double[a.length][a[0].length];
+		double[][] diagInv = new double[a.length][a[0].length];
 		double[] x1 = x;
 		double[][] low = a;
 		double[][] upp = a;
 		for(int i = 0; i < a.length; i++) {
 			for(int j =0; j < a[0].length; j++) {
 				if(i==j)  {
-					diag[i][j] = a[i][j];
-					upp[i][j] = a[i][j];
-					low[i][j] = a[i][j];
+					diagInv[i][j] = 1/a[i][j];
+					//upp[i][j] = a[i][j];
+					//low[i][j] = a[i][j];
 				}
 				if(i>j) {
 					upp[i][j] = a[i][j];
 					low[i][j] = 0.0;
-					diag[i][j] = 0.0;
+					diagInv[i][j] = 0.0;
 				} else {
 					low[i][j] = a[i][j];
 					upp[i][j] = 0.0;
-					diag[i][j] = 0.0;
+					diagInv[i][j] = 0.0;
 				}
 			}
 		}
@@ -31,7 +31,7 @@ public class JacobiIteration {
 			// x1 = Matrix.forwardSubstitution(diag, axplusb); 
 			//^fix that with however that is called in vishaaks's thing
 			double[] err = Matrix.subtract(x, x1);
-			double normerr = Matrix.norm(xminusx1);
+			double normerr = Matrix.norm(err);
 			if(normerr < tolerance) {
 				System.out.println("converged");
 				return x1;
